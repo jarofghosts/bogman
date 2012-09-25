e460764fdf19f33234753e34cd72f4ebfb334d73
jesse keane <me@jessekeane.me>
2012-09-24 22:13:37 -0700

    i have no idea what im doing

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem "sinatra", :require => "sinatra/base"
 gem "sinatra-activerecord"
+gem "rake"
 
 group :development, :test do
 	gem "sqlite3"
--- app.rb
+++ app.rb
@@ -2,6 +2,29 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './config/env'
 
+class Thing < ActiveRecord::Base
+  def self.whatis(thing_name)
+    where(:thing => thing_name).first
+  end
+end
+
 get '/' do
 	erb :index
+end
+
+post '/:thing/is/:description' do
+  thing = Thing.new do |t|
+    t.thing = params[:thing]
+    t.description = params[:description]
+  end
+  thing.save
+  redirect to( '/' + params[:thing] + '/is')
+end
+
+get '/:thing/is' do
+  Thing.whatis(params[:thing])
+end
+
+delete '*' do
+  "nope.avi"
 end
