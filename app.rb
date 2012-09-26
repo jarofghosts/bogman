419f1cfe1178f7de0b953d4c30c989f44820d9ac
jesse keane <me@jessekeane.me>
2012-09-25 22:16:56 -0700

    everybody is a star

--- app.rb
+++ app.rb
@@ -11,6 +11,21 @@ get '/' do
 	erb :index
 end
 
+get '/:number/things' do
+	number = params[:number] <= 50 ? params[:number] : 50;
+	Thing.take(number).to_json
+end
+
+get '/:number/old/things' do
+	number = params[:number] <= 50 ? params[:number] : 50;
+	Thing.first(number).to_json
+end
+
+get '/:number/new/things' do
+	number = params[:number] <= 50 ? params[:number] : 50;
+	Thing.last(number).to_json
+end
+
 post '/:thing/is/:definition' do
   thing = Thing.create( :thing => params[:thing], :definition => params[:definition]) unless Thing.exists?(:thing => params[:thing])
   Thing.where(:thing => params[:thing]).to_json
